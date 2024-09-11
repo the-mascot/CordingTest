8f32e238fde4cbb6a9df443bfe23bd6120a7959a
mascot <dmstn1812@naver.com>
2024-09-11 17:52:30 +0900

    삽입정렬, 병합정렬 예제 작성

---
+++ src/main/java/com/coding/codingtest/example/InsertionSortExample.java
@@ -0,0 +1,29 @@
+package com.coding.codingtest.example;
+
+import java.util.Arrays;
+
+/**
+ * 삽입정렬
+ * */
+public class InsertionSortExample {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 4, 1, 5, 9, 7, 6, 3, 2};
+
+        // KEY 는 두 번쨰 인덱스부터 시작
+        for (int i = 1; i < arr.length; i++) {
+            int keyValue = arr[i];
+            // 비교 인덱스
+            int j = i - 1;
+
+            // 비교 인덱스가 0이 될떄까지 && 비교인덱스가 키 값 보다 큰 경우
+            while (j >= 0 && arr[j] > keyValue) {
+                // 요소를 한칸 뒤로 이동
+                arr[j + 1] = arr[j];
+                j -= 1;
+            }
+            // 현재 위치에 key 삽입
+            arr[j + 1] = keyValue;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ src/main/java/com/coding/codingtest/example/MergeSortExample.java
@@ -0,0 +1,73 @@
+package com.coding.codingtest.example;
+
+import java.util.Arrays;
+
+/**
+ * 병합정렬
+ * */
+public class MergeSortExample {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 4, 1, 5, 9, 7, 6, 3, 2};
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        // 왼쪽 배열 개수
+        int n1 = mid - left + 1;
+        // 오른쪽 배열 개수
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // L 배열 생성
+        for (int i = 0; i < n1; i++) {
+            // 시작점 + i
+            L[i] = arr[left + i];
+        }
+
+        // R 배열 생성
+        for (int j = 0; j < n2; j++) {
+            // 시작점 + j
+            R[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = left;
+        // L과 R 의 원소를 비교하여 기존 배열 정렬
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // 나머지 배열 정리
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+
+    }
+}
