25671a8d114bfbf5682bff24e4994f8aa188d413
mascot <dmstn1812@naver.com>
2024-09-20 17:52:10 +0900

    디스크 컨트롤러 풀이중

--- src/main/java/com/coding/codingtest/heap/DiskController.java
+++ src/main/java/com/coding/codingtest/heap/DiskController.java
@@ -1,9 +1,10 @@
 package com.coding.codingtest.heap;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class DiskController {
     public static void main(String[] args) {
@@ -17,27 +18,28 @@ public class DiskController {
 
     public static int solution(int[][] jobs) {
         List<Integer> timeList = new ArrayList<>();
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
+        Queue<int[]> pq = new LinkedList<>();
+        Arrays.sort(jobs, (a, b) -> Integer.compare(a[0], b[0]));
 
         for (int[] job : jobs) {
             pq.offer(job);
         }
         int count = 0;
 
-        while (pq.isEmpty()) {
+        while (!pq.isEmpty()) {
             int[] job = pq.poll();
-            if (count - pq.peek()[0] < count - job[0]) {
+            if (pq.size() > 0 && Math.abs(count - pq.peek()[0]) < Math.abs(count - job[0])) {
                 pq.offer(job);
                 int[] nextJob = pq.poll();
                 count += nextJob[1];
                 timeList.add(count - nextJob[0]);
             } else {
                 count += job[1];
-                timeList.a
+                timeList.add(count - job[0]);
             }
         }
 
 
-        return answer;
+        return timeList.stream().mapToInt(Integer::intValue).sum() / timeList.size();
     }
 }
